a7f4fbe7393803591255bbcf3487ecce09ca36df
jhonny2sl <jhonnytwosl@gmail.com>
2022-12-05 15:26:12 +0700

    Create an Options Object

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './SearchBar.css';
+
+// Object contains requests for Yelp API
+// The default values follow formatting and naming convention set by the API: https://docs.developer.yelp.com/reference/v3_business_search > QUERY PARAMS > sort_by
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+};
