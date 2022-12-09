ffe00b8df41ee99fc91a79b774ed266c1724e094
jhonny2sl <jhonnytwosl@gmail.com>
2022-12-09 14:28:38 +0700

    Handle a Search

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -47,6 +47,11 @@ class SearchBar extends React.Component {
         });
     }
 
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
+    }
+
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             const sortByOptionValue = this.sortByOptions[sortByOption];
