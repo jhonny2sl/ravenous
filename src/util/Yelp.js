b62e2f5d768a2ee5bfb9e4434bd0781d2bd8f3fc
jhonny2sl <jhonnytwosl@gmail.com>
2022-12-13 14:22:28 +0700

    Add search()

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -4,3 +4,21 @@ const apiKey = '';
 const Yelp = {
 
 };
+
+// Method to retrieve search results from the Yelp API
+const search = (term, location, sortBy) => {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`
+    , {
+        headers: {
+            Authorization: `Bearer ${apiKey}`
+        }  
+    })
+    .then(response => response.json())
+    .then(jsonResponse => { 
+        if (jsonResponse.businesses) {
+            return jsonResponse.businesses.map(business => {
+                
+            });
+        }
+    });
+};
