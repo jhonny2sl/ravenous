ea76c976340011bd064094d93af20b07f8d4ee34
jhonny2sl <jhonnytwosl@gmail.com>
2022-12-13 13:21:19 +0700

    Create a Yelp Module

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,3 +1,6 @@
 // API key contains Yelp API v3 Credentials (make sure to leave it empty when this file is being shared or not in use)
 const apiKey = '';
+// This object will store the functionality needed to interact with the Yelp API
+const Yelp = {
 
+};
